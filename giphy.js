e853eea541ee6a197b35bd9055a7360554e3ec6c
Daria Korbysheva <korbyshevadaria@gmail.com>
2019-08-01 21:13:25 -0700

    working qoute api

--- giphy.js
+++ giphy.js
@@ -1,6 +1,7 @@
 /////// 1
 
  function giphyFunction() {
+  
 
     event.preventDefault();     
 
@@ -36,7 +37,36 @@
 
 
       }); // to close response funtion
-     
+      var queryURL = "http://quotes.rest/qod.json?category=inspire";
+    
+      // limit = 10 to limit the amount of gifs 
+   // console.log(queryURL);
+   $.ajax({
+     url: queryURL,
+     method: "GET"
+   }).then(function (response) {
+     console.log(queryURL);
+     console.log(response.contents.quotes[0].quote);
+
+
+     var quote = response.contents.quotes[0].quote;
+     var author = response.contents.quotes[0].author;
+
+     var results = quote+ " - " + author
+       // var quoteDiv = $("");
+       
+       // var quoteDiv = $("<img>");
+       // gifImage.addClass('gif');
+
+       // gifImage.attr("src", results[i].images.original.url);
+
+       
+       $("#quote-content").prepend(results);
+
+      
+
+
+   }); // to close response funti
     
   } // to close button function
 
